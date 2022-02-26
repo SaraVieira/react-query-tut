c1d5ba1980f72c40a4c488e53e87fce7f355207e
Sara Vieira <hey@iamsaravieira.com>
2022-02-26 16:49:09 +0100

    fix react coimplains

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -26,16 +26,15 @@ const Transaction = (transaction) => {
 
   return (
     <div
-      key={transaction._id}
-      class={classNames(
+      className={classNames(
         "ml-6 relative bg-white p-4  shadow-sm border-[1px] border-r-8 border-slate-100 my-4 flex justify-between rounded",
         transaction.amount > 0 ? "border-r-green-500" : "border-r-red-500"
       )}
     >
-      <div class="absolute -left-6">
+      <div className="absolute -left-6">
         <button
           onClick={deleteTransaction}
-          class="cursor-pointer bg-red-600 w-6 flex items-center text-white justify-center h-[30px] rounded-l"
+          className="cursor-pointer bg-red-600 w-6 flex items-center text-white justify-center h-[30px] rounded-l"
         >
           <XIcon />
         </button>
@@ -68,12 +67,12 @@ const Transaction = (transaction) => {
 
 export const Transactions = () => {
   return (
-    <div class="px-8 my-6">
-      <div class="my-4 border-b w-full">
-        <h2 class="font-semibold text-lg">History</h2>
+    <div className="px-8 my-6">
+      <div className="my-4 border-b w-full">
+        <h2 className="font-semibold text-lg">History</h2>
       </div>
       {transactions.map((transaction) => (
-        <Transaction {...transaction} />
+        <Transaction key={transaction._id} {...transaction} />
       ))}
     </div>
   );
